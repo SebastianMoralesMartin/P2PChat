19c066c7674bf5b5f7ce7cdd8575ede301dbe11c
Sebastian Morales Martin <sebastianmoralesm1999@outlook.com>
2019-11-04 17:30:24 -0600

    pre-alpha 0.0
    -bruh moment

--- Server.py
+++ Server.py
@@ -5,7 +5,7 @@ import threading
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('0.0.0.0', 8080))
-serv.listen(1)
+serv.listen(2)
 while True:
     conn, addr = serv.accept()
     from_client1 = []
@@ -15,6 +15,6 @@ while True:
             break
         from_client1.append(str(data))
         print(conn.getsockname() ,from_client1[-1])
-        #serv.send(bytes("I am SERVER\n", 'utf-8'))
+        conn.sendall(bytes("I am SERVER\n", 'utf-8'))
     conn.close()
     print('client disconnected')
