2570c6f461de5683c5a51d9a04e571a1274ef801
Eyri <leonardocastillejos03@gmail.com>
2019-11-04 20:21:47 -0600

    Intentemos esto

--- ServerLeo.py
+++ ServerLeo.py
@@ -2,13 +2,12 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
-print ("Haber si jala")
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        client.send(bytes("Type your name and press enter", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
